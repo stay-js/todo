abbdb5b02b354abe8c62c29f367f3aa81558d08f
stay <nagyzeteny6@gmail.com>
2022-07-19 21:52:23 +0200

    sign in

---
+++ src/components/LandingPage/SignIn.tsx
@@ -0,0 +1,24 @@
+import { signIn } from 'next-auth/react';
+import { FaGithub } from 'react-icons/fa';
+
+const SignIn: React.FC = () => (
+  <div
+    className="flex flex-col items-center justify-center w-screen gap-4"
+    style={{
+      height: 'calc(100vh - 4rem)',
+    }}
+  >
+    <h1 className="text-2xl font-bold">In order to use the application please sign in.</h1>
+
+    <button
+      type="button"
+      className="text-white bg-[#24292F] hover:bg-[#24292F]/90 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex items-center gap-2"
+      onClick={() => signIn('github')}
+    >
+      <FaGithub size={18} />
+      Sign in with Github
+    </button>
+  </div>
+);
+
+export default SignIn;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,30 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import { FaGithub } from 'react-icons/fa';
+import { signOut, useSession } from 'next-auth/react';
+import { SignIn } from '../components/LandingPage';
 
 const LandingPageContent: React.FC = () => {
-  const { data } = useSession();
+  const { data: session } = useSession();
 
-  if (!data) {
-    return (
-      <div
-        className="flex flex-col items-center justify-center w-screen gap-4"
-        style={{
-          height: 'calc(100vh - 4rem)',
-        }}
-      >
-        <h1 className="text-2xl font-bold">In order to use the application please sign in.</h1>
-
-        <button
-          type="button"
-          className="text-white bg-[#24292F] hover:bg-[#24292F]/90 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex items-center gap-2"
-          onClick={() => signIn('github')}
-        >
-          <FaGithub size={18} />
-          Sign in with Github
-        </button>
-      </div>
-    );
-  }
+  if (!session) return <SignIn />;
 
   return (
     <>
       <div className="relative flex items-center py-4 after:absolute after:block after:bg-neutral-700 after:w-full after:h-[1px] after:bottom-0 after:left-0">
         <div className="flex items-center justify-between w-full content">
           <div className="flex items-center gap-4">
-            {data.user?.image && (
+            {session.user?.image && (
               <Image
-                src={data.user?.image}
+                src={session.user?.image}
                 alt="GitHub Profile Picture"
                 width={48}
                 height={48}
                 className="rounded-full select-none"
               />
             )}
-            <p className="flex flex-col text-lg sm:gap-1 sm:flex-row">
-              Hi, <span className="font-semibold">{data.user?.name}!</span>
+            <p className="text-lg">
+              Hi, <span className="font-semibold">{session.user?.name}!</span>
             </p>
           </div>
 
