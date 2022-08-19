164f242229d1206e7efec58a69c44766b1718688
Nagy Zétény <nagyzeteny6@gmail.com>
2022-08-19 10:24:46 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*'],
   theme: {
     extend: {
       fontFamily: {
