5587f7fe559673394230b82018ab7da0623ca9dc
Nagy Zétény <nagyzeteny6@gmail.com>
2022-08-19 15:06:50 +0200

    Removed Roboto

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ['./src/**/*'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
