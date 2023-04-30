fe828bd1cd43ac3a5da8140df47a4352733383e7
Nagy Zétény <nagyzeteny6@gmail.com>
2023-04-30 16:26:08 +0200

    Update Meta.tsx

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -6,19 +6,26 @@ export const Meta: React.FC<{
   desc: string;
 }> = ({ path, title, desc }) => (
   <Head>
+    <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 
     <link rel="icon" href="/favicon.svg" />
 
     <meta name="author" content="Zétény Nagy" />
-    <meta name="keywords" content="todo, todo app, stay, znagy, znagy.hu, github" />
+
+    <meta
+      name="keywords"
+      content="todo, todo app, github, github auth, github authentication, stay, znagy, znagy.hu"
+    />
+
+    <meta name="theme-color" content="#22c55e" />
 
     <meta property="og:locale" content="en_US" />
-    <meta property="og:type" content="website" key="og_type" />
-    <meta property="og:site_name" content="todo" key="site_name" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Todo App" />
 
-    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:card" content="summary" />
     <meta property="twitter:domain" content="todo.znagy.hu" />
 
     <meta name="robots" content="index, follow" />
@@ -26,7 +33,7 @@ export const Meta: React.FC<{
     <meta name="rating" content="general" />
 
     <title>{title}</title>
-    <meta property="og:title" content={title} key="title" />
+    <meta property="og:title" content={title} />
     <meta name="twitter:title" content={title} />
 
     <meta name="description" content={desc} />
