5b25e033a604ec4ae9671708def4f41dc60a93cf
Nagy Zétény <nagyzeteny6@gmail.com>
2023-05-03 20:45:00 +0200

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ export const Button: React.FC<{
   return (
     <button
       type={type}
-      className={`whitespace-nowrap rounded border-2 px-4 py-2 text-sm font-bold text-white transition-all hover:bg-transparent
+      className={`h-fit w-fit whitespace-nowrap rounded border-2 px-4 py-2 text-sm font-bold text-white transition-all hover:bg-transparent
       ${
         color === 'red'
           ? 'border-red-500 bg-red-500 hover:text-red-500'
