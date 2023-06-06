d33b688b68ee6e3503166dccfbf0b585de6d454a
Nagy Zétény <nagyzeteny6@gmail.com>
2023-06-06 20:19:58 +0200

    redirect if session

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { signIn } from 'next-auth/react';
+import { useSession, signIn } from 'next-auth/react';
 import { FaGithub } from 'react-icons/fa';
 import { Meta } from '~/components/meta';
 
 const Page: NextPage = () => {
   const router = useRouter();
+  const { data: session } = useSession();
+
+  if (session) void router.push('/');
 
   return (
     <>
